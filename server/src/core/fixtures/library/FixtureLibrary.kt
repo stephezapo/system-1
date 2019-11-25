f8c3b0b53645ac119c19d2dd82438597e85f4b22
Stephan Zapotocky <zapotocky@zactrack.com>
2019-11-25 16:32:26 +0100

    create manufacturer/model database WIP

--- server/src/core/fixtures/library/FixtureLibrary.kt
+++ server/src/core/fixtures/library/FixtureLibrary.kt
@@ -4,7 +4,7 @@ import core.util.FileUtils
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.nio.file.Paths.*
+import javax.xml.parsers.DocumentBuilderFactory
 
 class FixtureLibrary(libraryDirectory: String) {
 
@@ -28,11 +28,35 @@ class FixtureLibrary(libraryDirectory: String) {
                parseFile(it)
             }
         }
+
+        //Iterate through extracted files and find manufacturer and model details in the respective description XML
+        val manufacturers = ArrayList<Manufacturer>()
+        val models = ArrayList<Model>()
+        File("$libraryDir/extracted").walk().forEach {
+            val xml = File(it.path + "/description.xml")
+            if(xml.exists()) {
+                parseXmlFile(xml, models)
+            }
+        }
     }
 
     private fun parseFile(file : File) {
         println("Parsing file ${file.name}")
 
         FileUtils.unzip(file.absolutePath, "$libraryDir/extracted/${file.nameWithoutExtension}")
+    }
+
+    private fun parseXmlFile(file : File, manufacturers: ArrayList<Manufacturer>, models: ArrayList<Model>) {
+        val dbFactory = DocumentBuilderFactory.newInstance()
+        val dBuilder = dbFactory.newDocumentBuilder()
+        val doc = dBuilder.parse(file)
+
+        val fTypeTag = doc.getElementsByTagName("FixtureType")
+        if(fTypeTag.length>0) {
+            val manufacturer = fTypeTag.item(0).attributes.getNamedItem("Manufacturer").nodeValue
+            //val model = fTypeTag.item(0).attributes.getNamedItem("Name").nodeValue
+            manufacturers.add(Manufacturer(manufacturer))
+            //models.add(Model(model))
+        }
     }
 }
