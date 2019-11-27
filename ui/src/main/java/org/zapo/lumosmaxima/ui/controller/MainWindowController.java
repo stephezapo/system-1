6f17401c5262b8036c5206ce1d96ae4baa30755d
Stephan Zapotocky <zapotocky@zactrack.com>
2019-11-27 16:45:35 +0100

    OpenJFX 13 / Java 13 based UI for luminosmaxima server. Showing 2 example screens

---
+++ ui/src/main/java/org/zapo/lumosmaxima/ui/controller/MainWindowController.java
@@ -0,0 +1,13 @@
+package org.zapo.lumosmaxima.ui.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainWindowController {
+
+    @FXML
+    private void test(ActionEvent event)
+    {
+        System.out.println("lollolol");
+    }
+}
